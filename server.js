d5ce90b917cf5ff1ea77c93f3923ac8c82ad961b
Andrew <pcgamesonline3@gmail.com>
2023-02-05 18:59:03 -0800

    Prepared app for deployment

--- server.js
+++ server.js
@@ -15,17 +15,33 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => console.log('DB connection successful!'));
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   })
+//   .then(() => console.log('DB connection successful!'));
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log(`MongoDB Connected`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+connectDB().then(() => {
+  const server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+  });
 });
 
 process.on('unhandledRejection', err => {
